956704ede9569ada035f13eefac09f64a0f86c16
Joseph Diza <josephm.diza@gmail.com>
2021-11-10 02:30:06 -0800

    Add setup.py for easy installation

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="bang-jmd",
+    version="0.1.0",
+    author="Joseph Diza",
+    author_email="josephm.diza@gmail.com",
+    description="An all purpose general templating program, and project starter program",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jmdaemon/bang",
+    project_urls={
+        "Bug Tracker": "https://github.com/jmdaemon/bang/issues",
+    },
+    license='MIT',
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.6",
+    py_modules=['bang'],
+    install_requires=[
+        'argparse',
+        'jinja2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'bang = bang:main',
+        ],
+    },
+    test_suite='tests',
+)
