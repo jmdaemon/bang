f163dc5a7da6cb8339a92060d0fbe7d4c63eea17
Joseph Diza <josephm.diza@gmail.com>
2021-12-11 08:02:06 -0800

    Add template aliases feature to bang

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ An all purpose templating program to quickly start projects off with a bang.
     using standard output
 3. Render the input to the template file using `[template_name].render`
 4. Write the output file to disk
+
+## Templates Aliases
+
+If you find that a path is too long to type out or that you always find yourself typing
+out the path to a certain template file, you can alias it using the `~/.config/bang/templates.json`
+file. There's an example `templates.json` included in the root direct of the repository.
--- src/bang.py
+++ src/bang.py
@@ -1,11 +1,22 @@
 import argparse
 import wora.dynmod
+import wora.file
+import json
+import pathlib
 
 def main():
     parser = argparse.ArgumentParser(description='An all purpose templating program to quickly start projects off with a bang')
     parser.add_argument('fp', type=str, help='Filepath to template directory')
     args = parser.parse_args()
     fp = args.fp
+
+    cfg = wora.file.read_file(pathlib.Path("~/.config/bang/templates.json").expanduser())
+    if (cfg is not None):
+        o = json.loads(cfg)
+        for name, tmplfp in o['templates'].items():
+            if fp == name:
+                fp = pathlib.Path(tmplfp).expanduser()
+                break
 
     template = wora.dynmod.module_from_file("template", f'{fp}/template.py')
     template.bang(fp)
