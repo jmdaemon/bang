c367f835f3b28459e928f790fe597d27b772084e
Joseph Diza <josephm.diza@gmail.com>
2021-12-16 16:28:46 -0800

    Create to_path

--- src/bang/tmpl.py
+++ src/bang/tmpl.py
@@ -3,11 +3,17 @@ from wora.file import read_file
 from pathlib import Path
 import plumbum
 
+def to_path(fp):
+    if isinstance(fp, Path):
+        return fp
+    elif isinstance(fp, str):
+        return Path(fp)
+
 def render(src, tmpl_name, vardict):
     tmpl = Template(read_file(str(Path(src) / tmpl_name)))
     res = tmpl.render(vardict)
     return res
 
 def output(dest, tmpl_name, tmpl):
-    dest = str(dest / tmpl_name)
+    dest = str(to_path(dest) / tmpl_name)
     (plumbum.cmd.echo[tmpl] > dest)()
